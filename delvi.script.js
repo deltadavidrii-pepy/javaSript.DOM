c2f06731169f5b43302b764f848058c339247271
pepy <deltadavidrii@gmail.com>
2026-03-20 19:26:28 +0700

    hasil8_javaSrip.DOM

--- delvi.script.js
+++ delvi.script.js
@@ -22,7 +22,36 @@ for (let i = 1; i <= dataUser.jml; i++) {
             </div>`;
     }
 
-    container2.innerHTML 
-)
+    container2.innerHTML =  container2.innerHTM + `<button id="btn-ok-2">OK</button>`;
 
-})
+    container2.classList.remove('hidden');
+
+
+    // Pasang event listener untuk tombol baru
+    document.getElementById('btn-ok-2').onclick = tahap2;
+
+
+});
+
+//-- tahap yang ke 2: klik OK yang ke dua (input pilihan)---
+
+function tahapke2(){
+let inputs = document.querySelectorAll('pilih- teks');
+dataUser.listPilihan =Array.from(inputs).map(input => input.value);
+
+     let adaYangKosong = false;
+
+for (let i = 0; i < dataUser.listPilihan.length; i++) {
+    if (dataUser.listPilihan[i] === "") {
+        adaYangKosong = true;
+        break; // Berhenti ngecek kalau sudah ketemu satu yang kosong
+    }
+}
+
+if (adaYangKosong) {
+    alert("Semua pilihan harus diisi teks!");
+    return;
+}
+
+}
+
